930d91f11977f20e9383017b5f30cf4244a7766f
Kim E <kim.e.swartz@gmail.com>
2023-10-02 21:48:34 +0200

    Task 4 Done

--- src/com/kimswartz/diceLab/GamePlay.java
+++ src/com/kimswartz/diceLab/GamePlay.java
@@ -8,28 +8,25 @@ public class GamePlay {
     // #1 We store players in an ArrayList:
     ArrayList<Player> ListOfPlayers = new ArrayList<>();
 
-    // #2: We create a six sided die object:
+    // #2: We create a six sided die object for players to roll:
     private Die dice = new Die();
 
-    // #3: We let user add players
+
+    // #3: Our method to let user add players and store it in our ArrayList:
     public void CreatePlayers() {
 
-        System.out.println("Would you like to add players? Enter Y / N");
-        String answer = scan.nextLine();
-
-        // We store user input names in our ArrayList as 'playerName':
-        while (answer.equalsIgnoreCase("y")) {
-            System.out.println("Enter the player name: ");
+        do {
+            System.out.println("Please enter the first player's name");
             String playerName = scan.nextLine();
             Player newPlayer = new Player(playerName);
             ListOfPlayers.add(newPlayer);
 
             System.out.println("Great, " + playerName + " is added! Would you like to add another player? Enter Y / N");
-            answer = scan.nextLine();
-        }
+        } while (scan.nextLine().equalsIgnoreCase("y"));
+
     }
 
-    // #4: We start the game by letting rolling the dice:
+    // #4: Our method to start the game by letting rolling the dice:
     protected void PlayDice() {
 
         /*  We for-loop through our ArrayList: ListOfPlayers to add and save the dice values */
@@ -44,12 +41,10 @@ public class GamePlay {
             player.setTotalScore(currentScore); // We save the value to setTotalScore-method in Player class
 
             System.out.println(player.getName() + " scored for a total of: " + player.getTotalScore() + " points!");
-
         }
-
     }
 
-    // #5 We determine and announce the winner:
+    // #5 Our method to determine and announce the winner:
     public void theWinner() {
 
         // We assume the 1:st player in our ArrayList is the initial winner.
@@ -61,6 +56,16 @@ public class GamePlay {
             }
         }
         System.out.println("The winner is: " + winner.getName() + " with a total score of " + winner.getTotalScore() + " points!");
+
+
+        /*
+
+        else {
+                System.out.println("It's a tie! Both players have a score of " + winner.getTotalScore() + ".");
+            }
+
+
+         */
 
     }
 
